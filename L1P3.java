dedb321cced7938fbac754190c1a6d9057c9c6c4
sai062188-rgb <sai062188@gmail.com>
2026-01-20 16:36:26 +0530

    Add files via upload

---
+++ L1P3.java
@@ -0,0 +1,16 @@
+
+import java.lang.*;
+class L1P3{
+	public static void main(String[]args){
+	int x = 10;
+	double d = 3.4;
+	char f = 'r';
+	float g = 25.55f;
+	String s = "HI";
+	Boolean t = true;
+	System.out.println("int = "+x +"\ndouble = "+d +"\nchar = "+f +"\nfloat = "+g +"\nString = "+s +"\nBoolean = "+t);
+
+	
+	
+	}
+	}
