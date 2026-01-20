91e5f418b42ed74e0ed70a71285caaa0af336a51
sai062188-rgb <sai062188@gmail.com>
2026-01-20 16:32:46 +0530

    Add files via upload

---
+++ LAB1/L1P2.java
@@ -0,0 +1,8 @@
+import java.lang.*;
+class L1P2{
+	public static void main(String[]args){
+	int x = 10; int y = 20;
+	double d = y;
+	System.out.println("Integer Value:"+x +" Double Value:"+y);
+}
+}
