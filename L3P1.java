064dc85a3019f8e13d443b4b4fc283a5d56c7b09
sai062188-rgb <sai062188@gmail.com>
2026-02-03 14:59:15 +0530

    Add files via upload

---
+++ L3P1.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+class L3P1{
+	static int view = 5;
+	public static void display(){
+	System.out.println(view);
+	}
+	public int show(){
+	return view;
+	}
+	public static void main(String[]args){
+	}
+
+}
+
+	
+	   
