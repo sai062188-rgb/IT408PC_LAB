48eba6825f6373808fd7f2e5c5887b6a015c0248
sai062188-rgb <sai062188@gmail.com>
2026-02-17 14:01:50 +0530

    Add files via upload

---
+++ LAB1/L1P3.java
@@ -0,0 +1,12 @@
+class L1P3{
+	public static void main(String...args){
+	int x = 10;
+	double d = 2.5;
+	char y = 't';
+	float f = 545.544f;
+	boolean g = true;
+	String s = "JAVA PROGRAMMING";
+	System.out.println("Integer = "+x+"\nDouble = "+d +"\nCharacter = "+y +"\nFloat = "+f +"\nBoolean = "+g +"\nString= "+s);
+	}}
+	
+	
